ea038fbcaa0b5515d8f31f58d7fa6e0fce05b400
rparekh01 <rajparekh001@outlook.com>
2025-04-20 04:07:02 -0400

    install components

---
+++ components/ui/login/login-page.tsx
@@ -0,0 +1,11 @@
+import { LoginForm } from "../auth/login-form";
+
+
+export default function LoginPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center px-4 py-12">
+      <LoginForm />
+    </div>
+  )
+}
+
